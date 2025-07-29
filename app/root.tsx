455a6c55a1b7e6f23ac1a2a19dbf28e10aa5802d
Artem Kartavtsev <arkartavtsev@gmail.com>
2025-07-29 20:15:30 +0700

    feat: Add contacts data

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,22 @@ import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
 
-export default function App() {
+import { getContacts } from "./data";
+
+
+export async function clientLoader() {
+  const contacts = await getContacts()
+
+  return { contacts }
+}
+
+
+export default function App({
+  loaderData
+}: Route.ComponentProps) {
+  const { contacts } = loaderData
+
+
   return (
     <>
       <div id="sidebar">
@@ -30,19 +45,39 @@ export default function App() {
             <button type="submit">New</button>
           </Form>
         </div>
+
         <nav>
-          <ul>
-            <li>
-              <Link to={ '/contacts/1' }>
-                Your Name
-              </Link>
-            </li>
-            <li>
-              <Link to={ '/contacts/2' }>
-                Your Friend
-              </Link>
-            </li>
-          </ul>
+          {
+            contacts.length ? <>
+              <ul>
+                {
+                  contacts.map(( contact ) => (
+                    <li key={ contact.id }>
+                      <Link to={ `contacts/${ contact.id }`} >
+                        {
+                          contact.first || contact.last ? <>
+                            { contact.first } { contact.last }
+                          </> : <>
+                            <i>No Name</i>
+                          </>
+                        }
+
+                        {
+                          contact.favorite ? <>
+                            <span>★</span>
+                          </> : null
+                        }
+                      </Link>
+                    </li>
+                  ))
+                }
+              </ul>
+            </> : <>
+              <p>
+                <i>No contacts</i>
+              </p>
+            </>
+          }
         </nav>
       </div>
 
