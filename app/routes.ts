e0f406554df2ce961af15c1a88c46333d648e364
Artem Kartavtsev <arkartavtsev@gmail.com>
2025-07-29 20:23:18 +0700

    feat: Add the index page rout

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,11 @@
 import {
   type RouteConfig,
+  index,
   route
 } from '@react-router/dev/routes'
 
 
 export default [
+  index('routes/home.tsx'),
   route('contacts/:contactId', 'routes/contact.tsx')
 ] satisfies RouteConfig
