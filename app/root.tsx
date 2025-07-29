1ec4016516b5b857f230c0cfb52db72cf6c25905
Artem Kartavtsev <arkartavtsev@gmail.com>
2025-07-29 20:18:23 +0700

    feat: Add a loader

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,18 @@ export async function clientLoader() {
   return { contacts }
 }
 
+export function HydrateFallback() {
+  return (
+    <div id='loading-splash'>
+      <div id='loading-splash-spinner' />
+
+      <p>
+        Loading, please wait...
+      </p>
+    </div>
+  )
+}
+
 
 export default function App({
   loaderData
