4564909a1eb7d3020e16496ed41e210b3c328eee
Artem Kartavtsev <arkartavtsev@gmail.com>
2025-08-04 01:58:46 +0700

    chore: Add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import path from 'path'
+
+import { defineConfig } from 'vite'
+import { reactRouter } from '@react-router/dev/vite'
+
 
 export default defineConfig({
   plugins: [reactRouter()],
   server: {
     port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
-});
+})
