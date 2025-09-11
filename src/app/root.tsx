7fdc8f490f1b84a6dddb4b2147e925426b09f58e
Artem Kartavtsev <arkartavtsev@gmail.com>
2025-09-11 15:48:30 +0700

    refactor: Update the root layout and  import of the styles

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,16 +1,18 @@
 import {
-  Outlet,
+  isRouteErrorResponse,
+  Links,
   Scripts,
   ScrollRestoration,
-  isRouteErrorResponse,
-} from "react-router";
-import type { Route } from "./+types/root";
+  Outlet,
+} from 'react-router'
 
-import appStylesHref from "./app.css?url";
+import type { Route } from './+types/root'
+
+import './app.css'
 
 
 export function HydrateFallback() {
-  return (
+  return <>
     <div id='loading-splash'>
       <div id='loading-splash-spinner' />
 
@@ -18,7 +20,63 @@ export function HydrateFallback() {
         Loading, please wait...
       </p>
     </div>
-  )
+  </>
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = 'Oops!'
+  let details = 'An unexpected error occurred.'
+  let stack: string | undefined
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? '404' : 'Error'
+    details =
+      error.status === 404
+        ? 'The requested page could not be found.'
+        : error.statusText || details
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message
+    stack = error.stack
+  }
+
+  return <>
+    <main id='error-page'>
+      <h1>{ message }</h1>
+      <p>{ details }</p>
+
+      {
+        stack && <>
+          <pre>
+            <code>{ stack }</code>
+          </pre>
+        </>
+      }
+    </main>
+  </>
+}
+
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return <>
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+
+        <title>React Router Contacts</title>
+        <meta name='description' content='A demo application showing off some of the powerful features of React Router' />
+
+        <Links /> 
+      </head>
+
+      <body>
+        { children }
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  </>
 }
 
 
@@ -26,57 +84,4 @@ export default function App() {
   return <>
     <Outlet />
   </>
-}
-
-// The Layout component is a special export for the root route.
-// It acts as your document's "app shell" for all route components, HydrateFallback, and ErrorBoundary
-// For more information, see https://reactrouter.com/explanation/special-files#layout-export
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>React Router Contacts</title>
-        <meta name="description" content="A demo application showing off some of the powerful features of React Router" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href={appStylesHref} />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-// The top most error boundary for the app, rendered when your app throws an error
-// For more information, see https://reactrouter.com/start/framework/route-module#errorboundary
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main id="error-page">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre>
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
 }
